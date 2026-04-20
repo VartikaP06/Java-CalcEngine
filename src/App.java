532813eac42dd4ae91b80b4eceeaaad81834dbaa
Vartika Priyadarshini <magicalvartika06.com>
2026-04-20 23:31:40 +0530

    Added logic to work with parallel arrays

--- src/App.java
+++ src/App.java
@@ -1,28 +1,31 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        var value1 = 100.0;
-        var value2 = 50.0;
-        var result = 0.0;
-        var opCode = 'd';
+        double[] leftVals = {100.0, 98.0, 209.0, 11.0};
+        double[] rightVals = {50.0, 23.0, 18.0, 3.0};
+        var results = new double[leftVals.length];
+        char[] opCodes = {'d', 'a', 's', 'm'};
 
-        switch (opCode) {
-            case 'a':
-                result = value1 + value2;
-                break;
-            case 's':
-                result = value1 - value2;
-                break;
-            case 'm':
-                result = value1 * value2;
-                break;
-            case 'd':
-                result = value2 != 0 ? value1 / value2 : 0.0;
-                break;
-            default:
-                result = 0.0;
-                System.out.println("Invalid OpCode");
+        for (int i = 0; i < leftVals.length; i++) {
+            switch (opCodes[i]) {
+                case 'a':
+                    results[i] = leftVals[i] + rightVals[i];
+                    break;
+                case 's':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+                case 'm':
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                case 'd':
+                    results[i] = rightVals[i] != 0 ? leftVals[i] / rightVals[i] : 0.0;
+                    break;
+                default:
+                    results[i] = 0.0;
+                    System.out.println("Invalid OpCode");
+            }
         }
-
-        System.out.println("Result is: " + result);
+        
+        for (var result: results)
+            System.out.println("Result is: " + result);
     }
 }
